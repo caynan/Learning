52d0b103f66558a7bf7798d0c9e1fd3709727029
Caynan Sousa <caynan.sousa@shopify.com>
2018-05-14 09:38:04 -0400

    added validations

--- Rails/rails-book/depot/app/models/product.rb
+++ Rails/rails-book/depot/app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true, length: {minimum: 10, message: 'description must be at least 10 characters long'}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, numericality: {
+    greater_than_or_equal_to: 0.01
+  }
 end
